dacd9b89b040f0c71ec0531a79722a110b5119f2
RT Software Studio <rt.sw.studio@gmail.com>
2020-02-07 22:21:10 +0200

    #5 RandomExtractConnector

---
+++ src/com/rtsw/openetl/agent/extract/RandomExtractConnector.java
@@ -0,0 +1,89 @@
+package com.rtsw.openetl.agent.extract;
+
+import com.rtsw.openetl.agent.api.AgentListener;
+import com.rtsw.openetl.agent.api.ExtractConnector;
+import com.rtsw.openetl.agent.common.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ *
+ * @author RT Software Studio
+ */
+public class RandomExtractConnector implements ExtractConnector {
+
+    private static final String ID = CSVExtractConnector.class.getName();
+
+    private Report report = new Report(ID);
+
+    private int minRows = 50;
+
+    private int maxRows = 100;
+
+    private int minValue = 50;
+
+    private int maxValue = 100;
+
+    @Override
+    public String getId() {
+        return (ID);
+    }
+
+    @Override
+    public void init(Configuration configuration) throws Exception {
+
+        report.start();
+
+        // optional
+        minRows = configuration.get("min_rows", 50);
+
+        // optional
+        maxRows = configuration.get("max_rows", 100);
+
+        // optional
+        minValue = configuration.get("min_value", 50);
+
+        // optional
+        maxValue = configuration.get("max_value", 100);
+
+    }
+
+    @Override
+    public void extract(AgentListener agentListener) {
+
+        Random random = new Random(System.currentTimeMillis());
+
+        List<Column> columns = new ArrayList<>();
+        columns.add(new Column("Index", "Integer", "java.lang.Integer"));
+        columns.add(new Column("Number", "Integer", "java.lang.Integer"));
+
+        Table table = new Table("RandomNumbers", columns);
+
+        agentListener.onTable(table);
+
+        for (int i = 0; i < random.nextInt(maxRows - minRows) + minRows; i++)  {
+
+            List<Object> values = new ArrayList<>();
+            values.add(i);
+            values.add(random.nextInt(maxValue - minValue) + minValue);
+
+            Row row = new Row(values);
+            agentListener.onRow(table, row);
+
+        }
+
+    }
+
+    @Override
+    public void clean() {
+        report.end();
+    }
+
+    @Override
+    public Report report() {
+        return(report);
+    }
+
+}
